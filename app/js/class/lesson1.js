98da8d55b689eb375bde691ef5ed6504116f6027
geyalan <490312867@qq.com>
2017-09-05 17:55:04 +0800

    es6 grammar

---
+++ app/js/class/lesson1.js
@@ -0,0 +1,22 @@
+function test(){
+	// for(var i=1;i<3;i++){
+	// 	console.log(i)
+	// }
+	// console.log(i)
+	// 1 2 3
+	
+	for(let i=1;i<3;i++){
+		console.log(i)
+	}
+	console.log(i)
+	//1 2 not defined
+	
+}
+
+function last(){
+	const PI=3.1415926;
+	console.log(PI);
+}
+
+test();
+last();
